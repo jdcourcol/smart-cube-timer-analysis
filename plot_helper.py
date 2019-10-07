54bb665764f9cbda96f88a681d7b7b0b50b5c092
jean-denis courcol <jean-denis.courcol@epfl.ch>
2019-10-07 20:45:25 +0200

    adding moves

--- plot_helper.py
+++ plot_helper.py
@@ -1,6 +1,6 @@
 def add_rolling(df, n):
     sn = str(n)
-    for c in ['time', 'f2l', '_pllTime', '_ollTime']:
+    for c in ['time', '_crossTime', 'f2l', 'pllTime', 'ollTime']:
         c_rolling = c.strip('_') + sn
         df [c_rolling] = df[c].rolling(n).mean()
         df [c_rolling + 'N'] = df[c_rolling] / df ['time' + sn]
@@ -8,9 +8,9 @@ def add_rolling(df, n):
 
 def plot_rolling(df, n):
     sn = str(n)
-    df[['time' + sn, 'f2l' + sn, 'pllTime' + sn, 'ollTime' + sn]].plot()
+    df[['time' + sn, 'crossTime' + sn, 'f2l' + sn, 'pllTime' + sn, 'ollTime' + sn]].plot()
 
 
 def plot_rollingN(df, n):
     sn = str(n)
-    df[['f2l' + sn + 'N', 'pllTime' + sn + 'N', 'ollTime' + sn + 'N']].plot()
+    df[['f2l' + sn + 'N', 'crossTime' + sn + 'N', 'pllTime' + sn + 'N', 'ollTime' + sn + 'N']].plot()
